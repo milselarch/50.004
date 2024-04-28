e4babb9495999c7f5e70dab4a44ef1f9f92b023b
milselarch <milselarch@gmail.com>
2024-04-28 23:04:35 +0800

    optimize sum tree dists

--- leetcode/sum_tree_dists.py
+++ leetcode/sum_tree_dists.py
@@ -19,16 +19,6 @@ class Solution:
             if tree_key in TREE_CACHE:
                 return TREE_CACHE[tree_key]
 
-            if block is not None:
-                generic_key = (start, None)
-
-                if generic_key in TREE_CACHE:
-                    main_size = TREE_CACHE[generic_key]
-                    cutoff_tree_size = get_tree_size(block, start)
-                    size = main_size - cutoff_tree_size
-                    TREE_CACHE[tree_key] = size
-                    return size
-
             size = 1
             neighbors = nmap[start]
 
@@ -42,24 +32,11 @@ class Solution:
             TREE_CACHE[tree_key] = size
             return size
 
-        def get_sum_dist(start, block=None):
+        def get_sum_dist(start, block=None, generalize=True):
             sum_key = (start, block)
-            print('KEY', sum_key, len(SUM_CACHE))
-
+            # print('KEY', sum_key)
             if sum_key in SUM_CACHE:
                 return SUM_CACHE[sum_key]
-
-            if block is not None:
-                generic_key = (start, None)
-
-                if generic_key in SUM_CACHE:
-                    main_sum = SUM_CACHE[generic_key]
-                    sub_sum = get_sum_dist(block, start)
-                    main_sum -= (
-                        sub_sum + get_tree_size(block, start)
-                    )
-                    SUM_CACHE[sum_key] = main_sum
-                    return main_sum
 
             total_sum = 0
             neighbors = nmap[start]
@@ -68,8 +45,13 @@ class Solution:
                 if neighbor == block:
                     continue
 
-                sub_sum = get_sum_dist(neighbor, start)
-                num_neighbors = get_tree_size(neighbor, start)
+                if (nmap[start] == {neighbor}) and generalize:
+                    num_neighbors = get_tree_size(neighbor, None) - 1
+                    sub_sum = get_sum_dist(neighbor, None, False) - 1
+                else:
+                    num_neighbors = get_tree_size(neighbor, start)
+                    sub_sum = get_sum_dist(neighbor, start)
+
                 total_sum += sub_sum + num_neighbors
 
             SUM_CACHE[sum_key] = total_sum
@@ -87,6 +69,8 @@ class Solution:
 if __name__ == '__main__':
     n = 30000
     edges = [[0, k] for k in range(n)]
+    # n = 2
+    # edges = [[0, 1]]
     ans = Solution.sum_of_distances_in_tree(n, edges)
     print('ANS', ans)
 
